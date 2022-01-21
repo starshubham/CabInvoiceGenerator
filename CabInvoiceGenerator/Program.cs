ed94b59dcfe84e6565c55b6e7a4593f9afe7b22c
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-21 22:57:25 +0530

    [add] Shubham added Program.cs file

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+
+            // UC1 : Total fare for single drive
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+            
+        }
+    }
+}
